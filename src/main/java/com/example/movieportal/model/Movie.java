fda081dbdc8afcfbec7f7173bad2d05c71fc6164
Jayagowtham <jayagowtham005@gmail.com>
2024-01-27 18:51:55 +0530

    first commit

---
+++ src/main/java/com/example/movieportal/model/Movie.java
@@ -0,0 +1,56 @@
+package com.example.movieportal.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Movie {
+	
+	@Id
+	private Integer movieId;
+	private String name;
+	private String hero;
+	private String director;
+	private String language;
+
+	public Integer getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(Integer movieId) {
+		this.movieId = movieId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHero() {
+		return hero;
+	}
+
+	public void setHero(String hero) {
+		this.hero = hero;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+}
---
+++ src/main/java/com/example/movieportal/repository/MovieRepository.java
@@ -0,0 +1,10 @@
+
+package com.example.movieportal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.movieportal.model.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+
+}
